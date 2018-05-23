2d96fb95d026d533afe191a8dbcd5fdd0cee7ea5
friedhelmensch <ffnoname@gmx.de>
2018-05-23 15:56:27 +0200

    create b2b user in end2end test

--- backend/_end2endTests/end2endTest.spec.js
+++ backend/_end2endTests/end2endTest.spec.js
@@ -1,4 +1,4 @@
-global.fetch = require('node-fetch');
+/*global.fetch = require('node-fetch');
 
 global.localStorage = {
     store: {},
@@ -20,12 +20,14 @@ global.window = global.window || {
     ArrayBuffer: global.ArrayBuffer,
     addEventListener: function () { },
     navigator: { onLine: true },
-    localStorage : global.localStorage
+    localStorage: global.localStorage
 };
-
+*/
 
 const expect = require('chai').expect;
-const Amplify = require("aws-amplify").default;
+//const Amplify = require("aws-amplify").default;
+const secrets = require("../secrets");
+var AWS = require('aws-sdk');
 
 describe('...', function () {
     this.timeout(15000);
@@ -43,31 +45,62 @@ describe('...', function () {
             }
         };
 
-        Amplify.configure({
-            Auth: {
-                mandatorySignIn: true,
-                region: config.cognito.REGION,
-                userPoolId: config.cognito.USER_POOL_ID,
-                identityPoolId: config.cognito.IDENTITY_POOL_ID,
-                userPoolWebClientId: config.cognito.APP_CLIENT_ID
-            },
-            API: {
-                endpoints: [
-                    {
-                        name: "apiKeys",
-                        endpoint: config.apiGateway.URL,
-                        region: config.apiGateway.REGION
-                    },
-                ]
-            }
-        })
-
         try {
-            const signInResult = await Amplify.Auth.signIn("hannes@rang.de", "passwordLol123!");
+
+            const tempPassword = "LolOlo123123!"
+
+            var params = {
+                UserPoolId: config.cognito.USER_POOL_ID,
+                Username: "nix@doesnotExist.iy",
+                DesiredDeliveryMediums: ["EMAIL"],
+                ForceAliasCreation: false,
+                TemporaryPassword: tempPassword,
+                UserAttributes: [
+                    { Name: "email", Value: "nix@doesnotExist.iy" },
+                    { Name: "email_verified", Value: "true" }
+                ]
+            };
+
+            var cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({ region: config.cognito.REGION, accessKeyId: secrets.awsAccessKeyId, secretAccessKey: secrets.awsSecretAccessKey});
+            const createUserResult = await cognitoIdentityServiceProvider.adminCreateUser(params).promise();
+
+            console.log(createUserResult);
+            /*
+            Amplify.configure({
+                Auth: {
+                    mandatorySignIn: true,
+                    region: config.cognito.REGION,
+                    userPoolId: config.cognito.USER_POOL_ID,
+                    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+                    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+                },
+                API: {
+                    endpoints: [
+                        {
+                            name: "apiKeys",
+                            endpoint: config.apiGateway.URL,
+                            region: config.apiGateway.REGION
+                        },
+                    ]
+                }
+            })
+
+            await Amplify.Auth.signIn("hannes@rang.de", "passwordLol123!");
+            
             const apiKeys = await Amplify.API.get("apiKeys", "/apiKeys");
-            console.log(apiKeys);
-        }
-        catch (e) {
+            const firstApiKey = apiKeys[0];
+
+            const apiKey = firstApiKey.apiKey;
+            const init = { headers: { 'x-api-key': apiKey } };
+            const getUsersResponse = await fetch("https://uxd0ifjso8.execute-api.us-east-1.amazonaws.com/dev/users", init)
+            const users = await getUsersResponse.json();
+            
+            users.forEach(user => {
+                console.log(user.UserAttributes[3].Value)
+            });*/
+
+        } catch (e) {
+            console.log("ERROR");
             console.log(e);
         }
     })
