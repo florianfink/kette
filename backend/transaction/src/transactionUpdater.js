5e219a5a32f12c2704c99837838db271ba1ae7de
Hannes Rang <hannes@rang.de>
2018-05-26 15:49:29 +0200

    introducing web hook for blockchain receipt

--- backend/transaction/src/transactionUpdater.js
+++ backend/transaction/src/transactionUpdater.js
@@ -10,8 +10,16 @@ module.exports.makeUpdateTransaction = (transactionRepository, secrets) => {
         if (!transaction) return { hasError: true, message: "secret does not match. " + "Transaction not found. Id: " + transactionId };
         if (transaction.blockchainRecordId !== blockchainReceipt.id) return { hasError: true, message: "transaction blockchainRecordId: " + transaction.blockchainId + " and blockchainreceipt id: " + blockchainReceipt.id + "not not match " }
 
-        return {transaction : transaction};
+        if (!transaction.receipts) {
+            transaction.receipts = [];
+        }
+        transaction.receipts.push(blockchainReceipt.receipt)
+        transaction.status = "confirmed";
+
+        await transactionRepository.save(transaction)
+
+        return { transaction: transaction };
     }
- 
+
     return updateTransaction
 }
