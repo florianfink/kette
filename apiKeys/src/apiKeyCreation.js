246f4790f8e7003388b0c0d409b594e6c1fe7af5
Hannes Rang <hannes@rang.de>
2018-08-02 16:09:11 +0200

    moving files back to

--- apiKeys/src/apiKeyCreation.js
+++ apiKeys/src/apiKeyCreation.js
@@ -9,9 +9,7 @@ exports.makeCreateApiKey = (deps) => {
 
         try {
             const userId = deps.extractUserId(cognitoAuthenticationProvider);
-            
             const apiKey = await deps.internalCreateApiKey();
-
             const apiKeyUserIdMapping = {
                 apiKey: apiKey.apiKey,
                 userId: userId,
@@ -20,6 +18,7 @@ exports.makeCreateApiKey = (deps) => {
             };
 
             await deps.apiKeyRepository.save(apiKeyUserIdMapping);
+
             return { apiKey: apiKey };
 
         } catch (error) {
