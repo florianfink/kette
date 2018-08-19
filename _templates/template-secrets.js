8126f9886e8933c679d1ce1bcfaa5d5911891f34
Hannes Rang <hannes@rang.de>
2018-08-19 11:48:57 +0200

    introducing secrets template for prod and dev

--- _templates/template-secrets.js
+++ _templates/template-secrets.js
@@ -1,4 +1,16 @@
-module.exports = {
-    stamperySecret : "_fill me in__",
-    ketteSecret : "_fill me in__"
-}
+const dev = {
+    stamperySecret : "_fill_me_in_dev",
+    ketteSecret : "_fill_me_in_dev"
+};
+
+const prod = {
+    stamperySecret : "_fill_me_in_prod",
+    ketteSecret : "_fill_me_in_prod"
+};
+
+// Default to dev if not set
+const config = process.env.STAGE === 'prod'
+    ? prod
+    : dev;
+
+module.exports = config;
